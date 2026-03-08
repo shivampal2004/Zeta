8214b71380a7bae992639951c969c7911220984f
Shivam Kumar <shivampal2004@gmail.com>
2026-03-08 16:08:33 +0530

    Merge Socket & Express Server

--- backend/src/controllers/socketManager.js
+++ backend/src/controllers/socketManager.js
@@ -6,7 +6,14 @@ let timeOnline = {}
 
 
 export const connectToSocket = (server) => {
-    const io = new Server(server);
+    const io = new Server(server, {
+        cors: {
+            origin: "*",
+            methods: ["GET", "POST"],
+            allowedHeaders: ["*"],
+            credentials: true
+        }
+    });
     io.on("connection", (socket) => {
 
         socket.on("join-call", (path) => {
@@ -57,7 +64,24 @@ export const connectToSocket = (server) => {
         })
 
         socket.on("disconnect", () => {
-            
+            let diffTime= Math.abs(timeOnline[socket.id]- new Date());
+            let key;
+            for(const [k, v] of JSON.parse(JSON.stringify(Object.entries(connections)))){
+                for(let a =0; a< v.length; ++a){
+                    if(v[a] === socket.id){
+                        key= k;
+                        for(let a=0; a< connections[key].length; ++a){
+                            io.to(connections[key][a]).emit('user-left', socket.id);
+                        }
+                        
+                        let index= connections[key].indexOf(socket.id);
+                        connections[key].splice(index, 1);
+                        if(connections[key].length === 0){
+                            delete connections[key];
+                        }
+                    }
+                }
+            }
         })
     })
     return io;
